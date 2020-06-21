603023abf6c953fbdd615499a042add84ae92193
Vipul Anand <va@vipulanand.in>
2020-06-21 16:59:03 +0530

    window opens only for domain

--- app.js
+++ app.js
@@ -264,3 +264,4 @@ window.addEventListener('userproximity', function(event) {
       navigator.mozPower.screenEnabled = true;
     }
   });
+  document.domain !== "www.morkhifarms.in"?window.close():""

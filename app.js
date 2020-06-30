b25250675eb45d53df95e23af34a6830fde42d9b
Vipul Anand <va@vipulanand.in>
2020-06-30 09:57:56 +0530

    Swipe Down to Refresh page
    Vanilla Js only

--- app.js
+++ app.js
@@ -397,3 +397,64 @@ window.addEventListener('userproximity', function(event) {
     }
   });
   //document.domain !== "www.morkhifarms.in"?window.close():""
+
+
+
+  // Swipe down to refresh implementation
+const pStart = { x: 0, y: 0 };
+const pCurrent = { x: 0, y: 0 };
+const cards = document.querySelectorAll(".farmProduce");
+const main = document.querySelector("body > div");
+let isLoading = false;
+
+function loading() {
+  isLoading = true;
+  main.style.transform = `translateY(0px)`;
+  setTimeout(() => {
+    main.style.transform = `translateY(-100px)`;
+    isLoading = false;
+    for (const card of cards) {
+      card.style.transform = `rotateX(0deg)`;
+    }
+  }, 2000);
+}
+
+function swipeStart(e) {
+  if (typeof e["targetTouches"] !== "undefined") {
+    let touch = e.targetTouches[0];
+    pStart.x = touch.screenX;
+    pStart.y = touch.screenY;
+  } else {
+    pStart.x = e.screenX;
+    pStart.y = e.screenY;
+  }
+}
+
+function swipeEnd(e) {
+  if (document.body.scrollTop === 0 && !isLoading) {
+    for (const card of cards) card.style.transform = `rotateX(0deg)`;
+  }
+}
+
+function swipe(e) {
+  if (typeof e["changedTouches"] !== "undefined") {
+    let touch = e.changedTouches[0];
+    pCurrent.x = touch.screenX;
+    pCurrent.y = touch.screenY;
+  } else {
+    pCurrent.x = e.screenX;
+    pCurrent.y = e.screenY;
+  }
+  let changeY = pStart.y < pCurrent.y ? Math.abs(pStart.y - pCurrent.y) : 0;
+  const rotation = changeY < 100 ? changeY * 30 / 100 : 30;
+  if (document.body.scrollTop === 0) {
+    if (changeY > 100) loading();
+    for (const card of cards) card.style.transform = `rotateX(${rotation}deg)`;
+  }
+}
+const refreshPage = () => {
+  location.reload();
+}
+document.addEventListener("touchstart", e => swipeStart(e), false);
+document.addEventListener("touchmove", e => {refreshPage(),swipe(e), false});
+document.addEventListener("touchend", e => swipeEnd(e), false);
